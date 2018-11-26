e71a97390e02649f3accc1afbe21f8b6ce3ccbe8
YossufAHem <45273413+YossufAHem@users.noreply.github.com>
2018-11-26 08:09:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -95,10 +95,44 @@ if (message.content.toLowerCase().startsWith(prefix + `c`)) {
 
 });
 
+const developers = ["408396389291393025"]
+const adminprefix = "-";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wat')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Status You   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'lis')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Status You  ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/gamer");
+      message.channel.send(`**Status You ${argresult} **`)
+ 
+    } else 
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
+});
+
 
 
 const sWlc = {}
-const premium = ['408396389291393025', '490803489765720064', '', '']
+const premium = ['408396389291393025']
 client.on('message', message => {
 var prefix = "p";
 if(message.channel.type === "dm") return;
