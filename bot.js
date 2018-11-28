4c2ef977fec66b088680d0b4caa5850a6e7ed7ba
YossufAHem <45273413+YossufAHem@users.noreply.github.com>
2018-11-28 08:29:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on("message", (message) => {
     .setTitle(`:mailbox_with_mail: Help`)
     .setColor(0xCF40FA)
     .setDescription(`هذا البوت للمساعدة فقط `)
-    .addField(`Tickets`, `[${prefix}b]() > **يفتح تذكرة جديدة ويضع علامات على فريق الدعم**\n[${prefix}c]() > **يغلق تذكرة تم حلها أو تم فتحها عن طريق الصدفة**
+    .addField(`Tickets`, `[${prefix}buy]() > **يفتح تذكرة جديدة ويضع علامات على فريق الدعم**\n[${prefix}c]() > **يغلق تذكرة تم حلها أو تم فتحها عن طريق الصدفة**
 `)
     .addField(`Invite`, `[${prefix}Invites]() > **لمعرفة كام دخلت اشخاص**\n[${prefix}invite-codes]() > **لمعرفة اكواد الني صنعطها**
 `)
